d445ade8bc749312fcde6ec12688b371e233f3d3
Andy Weng <andyweng257@gmail.com>
2025-09-17 07:16:20 -0700

    Update dictonaries.py

---
+++ dictonaries.py
@@ -0,0 +1,21 @@
+# healthhold = True
+# withadult = False
+# Tallreq = True
+# def check():
+#     if not healthhold == True:
+#         if withadult or Tallreq:
+#             print("You may ride the rollarcoster")
+#         else:
+#             print("No adult or tall requirement seen.CANT RIDE")
+#     else:
+#         print("Sorry, You can not be on the coaster.")
+
+# check()
+
+
+Items = { 
+    "Fan":""
+}
+
+
+
