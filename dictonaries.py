7272588cc015e398142f5a9b23e5e6fb8dc7491d
Andy Weng <andyweng257@gmail.com>
2025-09-18 07:16:15 -0700

    Update dictonaries.py

--- dictonaries.py
+++ dictonaries.py
@@ -1,21 +1,7 @@
-# healthhold = True
-# withadult = False
-# Tallreq = True
-# def check():
-#     if not healthhold == True:
-#         if withadult or Tallreq:
-#             print("You may ride the rollarcoster")
-#         else:
-#             print("No adult or tall requirement seen.CANT RIDE")
-#     else:
-#         print("Sorry, You can not be on the coaster.")
-
-# check()
-
-
-Items = { 
-    "Fan":""
-}
-
-
-
+Grocery_Store = [
+    {}
+        "Apple":1,
+        "Banana":2,
+        "Strawberry":1
+    
+]
